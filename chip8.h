0934c60cfe5e7ee35596a298a7267a45a374ba60
William Zane Martin-Chan <wm2marti@uwaterloo.ca>
2020-06-30 13:17:33 -0400

    removed SDL from chip8.h. Conceptually, graphics should be seperated from the emulation. Main will handle graphics and input, neccessary information will be passed between them. This makes it much easier to change graphics libraries as desired.

--- chip8.h
+++ chip8.h
@@ -9,29 +9,24 @@
 #define STACK_SIZE 16
 
 struct Chip8 {
+    unsigned short opcode; 				// current OPCODE
+	unsigned short I;					// I register for holding addr
+	unsigned short pc;					// Program Counter
+	unsigned char memory[MEMORY_SIZE];	// Memory, code starting @addr hex 200 == dec 512
+	unsigned char V[16];				// Usable registers
 
-    //
-    unsigned short opcode;
-	unsigned short I;
-	unsigned short pc;
-	unsigned char memory[MEMORY_SIZE];
-	unsigned char V[16];
+	signed char waiting;				// flag asking to wait on a keypress
 
-	signed char waiting;
-
-	unsigned short drawFlag;
+	unsigned short drawFlag;			// Indicates the screen info has changed and needs to be redrawn
 	unsigned char gfx[SCREEN_WIDTH * SCREEN_HEIGHT];         // Stores current screen state
 
 	unsigned char delay_timer;
 	unsigned char sound_timer;
 
-	unsigned short stack[STACK_SIZE];
-	short sp; //stack ptr
+	unsigned short stack[STACK_SIZE];	// Call stack stores return addr
+	short sp; 							// stack ptr
 
-	unsigned char key[16];
-
-	SDL_Window* window;
-	SDL_Renderer* renderer;
+	unsigned char key[16];				// key states
 };
 
 void initialize(struct Chip8* chip);
